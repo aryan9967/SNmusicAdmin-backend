e8e472a19d5dd144fa819397cab7624fc94c3b7e
Simran Sharma <simran.s.sharma00@gmail.com>
2024-07-22 21:54:15 +0530

    Changing branch

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -181,7 +181,7 @@ export const createAlbumItem = async (req, res) => {
     await createSubData(process.env.albumFolderCollection, process.env.albumItemCollection, albumFolderId, albumItemId, albumJson);
 
     cache.del('album_item');
-    
+
     res.status(201).send({
       success: true,
       message: 'Album created successfully',
@@ -297,24 +297,14 @@ export const readSingleAlbumFolder = async (req, res) => {
         {
           "albumFolderId": "686554da-5cbb-4200-893a-06a04decaf5f",
           "imageUrl": "bbnbn.com",
-          "mediaUrl": "https://firebasestorage.googleapis.com/v0/b/snmusic-ca00f.appspot.com/o/albums%2F686554da-5cbb-4200-893a-06a04decaf5f%2F8a9ebb00-714f-4ed7-8e89-161e35b38802%2Flogin3c.jpg?alt=media&token=e17fe9f8-1809-4bd5-8e17-1f0f21b91afb",
           "albumItemId": "8a9ebb00-714f-4ed7-8e89-161e35b38802",
           "title": "title1",
-          "timestamp": {
-            "_seconds": 1721331729,
-            "_nanoseconds": 532000000
-          }
         },
         {
           "albumFolderId": "686554da-5cbb-4200-893a-06a04decaf5f",
           "imageUrl": "bbnbn.com",
-          "mediaUrl": "https://firebasestorage.googleapis.com/v0/b/snmusic-ca00f.appspot.com/o/albums%2F686554da-5cbb-4200-893a-06a04decaf5f%2Fbada917a-ca12-4233-a1fe-c1068b6eed3b%2Flogin3c.jpg?alt=media&token=bce53592-88bb-4d11-947f-e1662991f744",
           "albumItemId": "bada917a-ca12-4233-a1fe-c1068b6eed3b",
           "title": "title2",
-          "timestamp": {
-            "_seconds": 1721332325,
-            "_nanoseconds": 935000000
-          }
         }
         }
       ]
@@ -563,7 +553,7 @@ export const updateAlbumItem = async (req, res) => {
   }
 };
 
-//function to update single our Students details
+//function to delete single our album details
 /* 
     request url = http://localhost:8080/api/v1/album/delete-album-folder
     method = POST
@@ -632,7 +622,7 @@ export const deleteAlbumItem = async (req, res) => {
 
     var albumData = await deleteSubData(process.env.albumFolderCollection, process.env.albumItemCollection, albumFolderId, albumItemId);
     console.log('success');
-    
+
     cache.del('album_item');
 
     return res.status(201).send({
